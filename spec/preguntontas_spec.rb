bbffdfcfa70f07a4ad6bf8954bd16a9b4b9c515e
vagrant <vagrant@kleer-csd-box>
2019-11-14 08:58:32 -0300

    Arreglo rspec

--- spec/preguntontas_spec.rb
+++ spec/preguntontas_spec.rb
@@ -9,12 +9,12 @@ describe "Partida preguntontas" do
   it "Respuesta correcta" do
     partida = Partida_preguntontas.new
     partida.validar 0
-    expect(partida.resultado).to eq 1
+    expect(partida.resultado) == "BIEN"
   end
 
   it "Respuesta incorrecta" do
     partida = Partida_preguntontas.new
     partida.validar 3
-    expect(partida.resultado).to eq 0
+    expect(partida.resultado) == "MAL"
   end
 end
